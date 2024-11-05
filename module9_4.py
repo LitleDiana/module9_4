01c750a44c126d9abde8efa8b35a67cd1187d142
Диана <diana_ahmedyanov@mail.ru>
2024-11-05 22:15:21 +0300

    5/11

--- module9_4.py
+++ module9_4.py
@@ -13,3 +13,16 @@ def get_advanced_writer(file_name):
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+from random import choice
+class MysticBall:
+    def __init__(self,*word):
+        self.word = word
+
+    def __call__(self):
+        return choice(self.word)
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
+
